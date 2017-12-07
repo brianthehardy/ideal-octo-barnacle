eaf2472f3053e9c41ce45b0c625e298e840c1b53
brianthehardy <brian.the.hardy@gmail.com>
2017-12-07 15:25:50 -0500

    Added the .java files

---
+++ AnimalTester.java
@@ -0,0 +1,12 @@
+
+public class AnimalTester {
+
+	public static void main(String[] args) {
+		// TODO create a lion object, e.g., Animal lion = new Animal(...);
+		
+		// TODO create an owner for the lion e.g., Owner owner = new Owner(...);
+		
+		// TODO print the name of the owner and the lion to the console
+		
+	}
+}
---
+++ Owner.java
@@ -0,0 +1,35 @@
+
+public class Owner {
+
+	private String name;
+	private Animal animal;
+	
+	/*
+	 * Constructor of the class
+	 * @param name name of the owner
+	 * @param animal animal owned
+	 */
+	public Owner(String name, Animal animal) {
+		// TODO complete the method
+	}
+	
+	/*
+	 * Getter for the animal owned
+	 * @return the animal
+	 */
+	public Animal getAnimal() {
+		// TODO complete the method
+		return null;
+	}
+	
+	/*
+	 * Setter for the animal owned
+	 * @return the animal
+	 */
+	public void setAnimal() {
+		// TODO complete the method
+		return;
+	}
+	
+	
+}
